640318c1d487631fa192bd92bda0f1f7b605071e
otavioaraujo08 <otavioaraujo490@gmail.com>
2025-01-02 13:51:48 -0300

    feat: enhance Login component with improved styling and layout

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,6 +1,7 @@
 import { useContext, useState } from 'react';
 import { useNavigate } from 'react-router';
 import { AuthContext } from '../../context/AuthContext';
+import './index.css';
 
 export const Login = () => {
   const authContext = useContext(AuthContext);
@@ -20,25 +21,43 @@ export const Login = () => {
   };
 
   return (
-    <div>
-      <h1>Login</h1>
-      <input
-        type="email"
-        placeholder="Email"
-        value={userInfos.userName}
-        onChange={(e) =>
-          setUserInfos({ ...userInfos, userName: e.target.value })
-        }
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={userInfos.password}
-        onChange={(e) =>
-          setUserInfos({ ...userInfos, password: e.target.value })
-        }
-      />
-      <button onClick={handleLogin}>Login</button>
+    <div className="login">
+      <div className="flex flex-col items-center justify-center p-8 rounded shadow-md min-h-[30rem] gap-7">
+        <h1 className="title">Vault Games</h1>
+        <div>
+          <label className="text-white text-[1.2rem] font-bold font-sans">
+            Usuário
+          </label>
+          <input
+            type="email"
+            placeholder="Usuário"
+            value={userInfos.userName}
+            onChange={(e) =>
+              setUserInfos({ ...userInfos, userName: e.target.value })
+            }
+            className="mb-4 p-2 border rounded w-full"
+          />
+
+          <label className="text-white text-[1.2rem] font-bold font-sans">
+            Senha
+          </label>
+          <input
+            type="password"
+            placeholder="Senha"
+            value={userInfos.password}
+            onChange={(e) =>
+              setUserInfos({ ...userInfos, password: e.target.value })
+            }
+            className="mb-4 p-2 border rounded w-full"
+          />
+        </div>
+        <button
+          onClick={handleLogin}
+          className="bg-blue-700 text-white p-2 rounded w-full"
+        >
+          Login
+        </button>
+      </div>
     </div>
   );
 };
