28cd6a7ac7a699c5a3128afc1f0a3cf4c6aefd0e
otavioaraujo08 <otavioaraujo490@gmail.com>
2025-01-02 11:27:35 -0300

    remove unused App.css file and clean up imports in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import './App.css';
 import { Login } from './pages/auth/Login';
 import { Dashboard } from './pages/dashboard/Dashboard';
 
