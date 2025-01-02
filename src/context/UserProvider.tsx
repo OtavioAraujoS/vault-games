eeddf1856765b5ef95153bcfb19c8cabab71a554
otavioaraujo08 <otavioaraujo490@gmail.com>
2025-01-02 13:01:07 -0300

    feat: implement UserProvider for managing user state and authentication

---
+++ src/context/UserProvider.tsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import { User } from '../types/User';
+import { AuthContext } from './AuthContext';
+
+export const UserProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const [user, setUser] = useState<User | null>(null);
+
+  const login = (userData: User) => {
+    setUser(userData);
+  };
+
+  const logout = () => {
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
