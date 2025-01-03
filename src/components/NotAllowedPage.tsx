a163b5c0a20542d8dce88e005a318b31c636dea5
otavioaraujo08 <otavioaraujo490@gmail.com>
2025-01-03 14:15:49 -0300

    feat: enhance NotAllowedPage to include logout functionality and navigation

--- src/components/NotAllowedPage.tsx
+++ src/components/NotAllowedPage.tsx
@@ -1,11 +1,24 @@
+import { AuthContext } from '@/context/AuthContext';
 import { Angry } from 'lucide-react';
+import { useContext } from 'react';
+import { useNavigate } from 'react-router';
 import { Button } from './ui/button';
 
-interface NotAllowedPageProps {
-  onClick: () => void;
-}
+export const NotAllowedPage = () => {
+  const authContext = useContext(AuthContext);
+  const navigate = useNavigate();
 
-export const NotAllowedPage = ({ onClick }: NotAllowedPageProps) => {
+  if (!authContext) {
+    return <div>Loading...</div>;
+  }
+
+  const { logout } = authContext;
+
+  const LogOut = () => {
+    logout();
+    navigate('/');
+  };
+
   return (
     <div className="bg-[#dad8d8] flex flex-col justify-center items-center gap-4 w-full h-full min-h-[100vh]">
       <Angry className="text-red-600 w-fit h-[6rem] md:h-[8rem] lg:h-[10rem]" />
@@ -15,7 +28,7 @@ export const NotAllowedPage = ({ onClick }: NotAllowedPageProps) => {
       </h1>
       <Button
         className="w-fit p-2 min-w-[8rem] md:min-w-[10rem] lg:min-w-[15rem] bg-red-600 hover:bg-red-700"
-        onClick={onClick}
+        onClick={LogOut}
       >
         Realizar Login
       </Button>
