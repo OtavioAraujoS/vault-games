c9a830e6ed26acbdd404be4ff30644dc6209fd02
otavioaraujo08 <otavioaraujo490@gmail.com>
2025-01-03 14:16:06 -0300

    feat: add NotAllowedPage route for handling unauthorized access

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { NotAllowedPage } from './components/NotAllowedPage';
 import { Login } from './pages/auth/Login';
 import { Dashboard } from './pages/dashboard/Dashboard';
 
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="*" element={<NotAllowedPage />} />
       </Routes>
     </Router>
   );
