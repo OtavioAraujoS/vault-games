bb8a25e32ee71103b29487d1036efe4a91e93fed
otavioaraujo08 <otavioaraujo490@gmail.com>
2025-01-07 09:50:11 -0300

    feat: enhance Header component with footer and layout adjustments

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -10,16 +10,35 @@ const Header: React.FC = () => {
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <SidebarProvider>
         <Sidebar />
-        <div className="h-full w-full">
+        <div className="h-full min-h-[100vh] w-full">
           <header className="bg-white dark:bg-gray-800 bg-gray border-b border-gray-400 text-black dark:text-white p-4 flex justify-between items-center">
-            <SidebarTrigger />
+            <SidebarTrigger className="size-10 dark:bg-gray-800 border border-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-500" />
             <ThemeToggle />
           </header>
           <main>
             <Outlet />
           </main>
-          <footer>
-            <p>Dashboard Footer</p>
+          <footer className="flex justify-center text-center bg-white dark:bg-gray-800 bg-gray border-t border-gray-400 text-black dark:text-white p-4 absolute bottom-0 w-full">
+            <p>
+              &copy; {new Date().getFullYear()} Built by{' '}
+              <a
+                href="https://github.com/otavioaraujo08"
+                target="_blank"
+                rel="noreferrer"
+                className="text-blue-500"
+              >
+                Otávio Araujo.
+              </a>{' '}
+              The source code is available on{' '}
+              <a
+                href="https://github.com/otavioaraujo08/vault-games"
+                target="_blank"
+                rel="noreferrer"
+                className="text-blue-500"
+              >
+                GitHub.
+              </a>
+            </p>
           </footer>
         </div>
       </SidebarProvider>
