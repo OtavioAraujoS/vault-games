147d4d15009a9951aeb1c1ba05b97209ade8c003
otavioaraujo08 <otavioaraujo490@gmail.com>
2025-01-07 13:49:53 -0300

    refactor: update route to use Layout component instead of Header for consistency

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import { NotAllowedPage } from './components/NotAllowedPage';
-import Header from './layouts/Header';
+import Layout from './layouts/Layout';
 import { Login } from './pages/auth/Login';
 import { Dashboard } from './pages/dashboard/Dashboard';
 
@@ -9,7 +9,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Header />}>
+        <Route path="/dashboard" element={<Layout />}>
           <Route index element={<Dashboard />} />
         </Route>
         <Route path="*" element={<NotAllowedPage />} />
