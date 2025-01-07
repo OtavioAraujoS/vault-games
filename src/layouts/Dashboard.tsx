8813f00f4be81da8000bd06c7537fec6de695c44
otavioaraujo08 <otavioaraujo490@gmail.com>
2025-01-07 09:01:12 -0300

    feat: update Dashboard header styles for improved visibility and accessibility

--- src/layouts/Dashboard.tsx
+++ src/layouts/Dashboard.tsx
@@ -7,7 +7,7 @@ const Header: React.FC = () => {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <div className="h-full w-full">
-        <header className="bg-gray-800 text-white p-4 flex justify-between items-center">
+        <header className="bg-white dark:bg-gray-800 bg-gray border border-gray-800 text-black dark:text-white p-4 flex justify-between items-center">
           <h1 className="font-bebas text-[1.6rem] tracking-wider">
             VAULT GAMES
           </h1>
