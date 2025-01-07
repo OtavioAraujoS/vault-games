77e77fca5e892baabaa40a525ed3633152077a60
otavioaraujo08 <otavioaraujo490@gmail.com>
2025-01-07 08:56:33 -0300

    feat: add Dashboard layout with theme provider and toggle component

---
+++ src/layouts/Dashboard.tsx
@@ -0,0 +1,27 @@
+import { ThemeToggle } from '@/components/ThemeToggle';
+import { ThemeProvider } from '@/context/ThemeContext';
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+
+const Header: React.FC = () => {
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div className="h-full w-full">
+        <header className="bg-gray-800 text-white p-4 flex justify-between items-center">
+          <h1 className="font-bebas text-[1.6rem] tracking-wider">
+            VAULT GAMES
+          </h1>
+          <ThemeToggle />
+        </header>
+        <main>
+          <Outlet />
+        </main>
+        <footer>
+          <p>Dashboard Footer</p>
+        </footer>
+      </div>
+    </ThemeProvider>
+  );
+};
+
+export default Header;
