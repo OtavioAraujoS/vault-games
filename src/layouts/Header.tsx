dffba1ad71e5a376b9575558d06350d630ea8abd
otavioaraujo08 <otavioaraujo490@gmail.com>
2025-01-07 09:24:21 -0300

    feat: integrate Sidebar component with Header for improved navigation

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,25 +1,28 @@
 import { ThemeToggle } from '@/components/ThemeToggle';
+import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
 import { ThemeProvider } from '@/context/ThemeContext';
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import { Sidebar } from './Sidebar';
 
 const Header: React.FC = () => {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <div className="h-full w-full">
-        <header className="bg-white dark:bg-gray-800 bg-gray border border-gray-800 text-black dark:text-white p-4 flex justify-between items-center">
-          <h1 className="font-bebas text-[1.6rem] tracking-wider">
-            VAULT GAMES
-          </h1>
-          <ThemeToggle />
-        </header>
-        <main>
-          <Outlet />
-        </main>
-        <footer>
-          <p>Dashboard Footer</p>
-        </footer>
-      </div>
+      <SidebarProvider>
+        <Sidebar />
+        <div className="h-full w-full">
+          <header className="bg-white dark:bg-gray-800 bg-gray border-b border-gray-400 text-black dark:text-white p-4 flex justify-between items-center">
+            <SidebarTrigger />
+            <ThemeToggle />
+          </header>
+          <main>
+            <Outlet />
+          </main>
+          <footer>
+            <p>Dashboard Footer</p>
+          </footer>
+        </div>
+      </SidebarProvider>
     </ThemeProvider>
   );
 };
