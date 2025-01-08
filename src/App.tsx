14ffbcc2859b01d9539dd4c1cf9b1cb8443ef75f
otavioaraujo08 <otavioaraujo490@gmail.com>
2025-01-08 13:43:30 -0300

    feat: update routing to include Jogos page and restructure routes for clarity

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,19 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import { NotAllowedPage } from './components/NotAllowedPage';
 import Layout from './layouts/Layout';
 import { Login } from './pages/auth/Login';
-import { Dashboard } from './pages/dashboard/Dashboard';
+import Dashboard from './pages/dashboard/Dashboard';
+import { Jogos } from './pages/jogos/Jogos';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Layout />}>
-          <Route index element={<Dashboard />} />
+        <Route index element={<Login />} />
+        <Route element={<Layout />}>
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="jogos" element={<Jogos />} />
+          <Route path="*" element={<NotAllowedPage />} />
         </Route>
-        <Route path="*" element={<NotAllowedPage />} />
       </Routes>
     </Router>
   );
