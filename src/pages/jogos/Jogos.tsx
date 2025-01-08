bfa26ef0a460a4b4df2588690155d3407ba73d2b
otavioaraujo08 <otavioaraujo490@gmail.com>
2025-01-08 13:43:04 -0300

    feat: add Jogos component for displaying game titles

---
+++ src/pages/jogos/Jogos.tsx
@@ -0,0 +1,7 @@
+export const Jogos = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-full">
+      <h1 className="text-3xl font-bold">Jogos</h1>
+    </div>
+  );
+};
