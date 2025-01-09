8acdc1aee99b2cb6d762a75def34b4433ff2138a
otavioaraujo08 <otavioaraujo490@gmail.com>
2025-01-09 10:48:51 -0300

    feat: add UnlinkMessage component for displaying alert messages

---
+++ src/components/UnlinkMessage.tsx
@@ -0,0 +1,25 @@
+import { CircleAlert } from 'lucide-react';
+import type { ReactNode } from 'react';
+
+interface UnlinkMessageProps {
+  title?: string;
+  children?: ReactNode;
+}
+
+export const UnlinkMessage = ({ title, children }: UnlinkMessageProps) => {
+  return (
+    <div className="my-4 flex flex-col items-center justify-center gap-2 text-center">
+      <CircleAlert color="red" size={100} />
+      <h1 className="text-[1.8rem] tracking-wide md:text-[2rem] lg:text-[2.4rem]">
+        Atenção
+      </h1>
+      {title ? (
+        <p className="max-w-[52rem] text-[1rem] md:text-[1.2rem] lg:text-[1.4rem]">
+          {title}
+        </p>
+      ) : (
+        children
+      )}
+    </div>
+  );
+};
