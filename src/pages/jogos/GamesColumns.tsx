29cff3b04b209f0e711c4bdb10009c6fee771969
otavioaraujo08 <otavioaraujo490@gmail.com>
2025-01-09 11:21:23 -0300

    feat: replace Columns component with GamesColumns for improved game data display

---
+++ src/pages/jogos/GamesColumns.tsx
@@ -0,0 +1,69 @@
+import { Dialog } from '@/components/Dialog';
+import { StatusChip } from '@/components/StatusChip';
+import { UnlinkMessage } from '@/components/UnlinkMessage';
+import { Game } from '@/types/Games';
+import type { ColumnDef } from '@tanstack/react-table';
+import { Trash } from 'lucide-react';
+
+export const GamesColumns: ColumnDef<Game>[] = [
+  {
+    accessorKey: 'id',
+    header: () => {
+      return <div className="ml-24 dark:text-white">Id</div>;
+    },
+    cell: ({ row }) => {
+      return <div className="ml-4 dark:text-white">{row.original._id}</div>;
+    },
+  },
+  {
+    accessorKey: 'nome',
+    header: () => {
+      return <div className="dark:text-white">Nome</div>;
+    },
+    cell: ({ row }) => {
+      return <div className="dark:text-white">{row.original.nome}</div>;
+    },
+  },
+  {
+    accessorKey: 'status',
+    header: () => {
+      return <div className="ml-14 dark:text-white">Status</div>;
+    },
+    cell: ({ row }) => {
+      return (
+        <div>
+          <StatusChip status={row.original.status} />
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: 'options',
+    header: () => {
+      return <div className="dark:text-white">Opções</div>;
+    },
+    cell: ({ row }) => {
+      return (
+        <div className="dark:text-white">
+          <Dialog
+            buttonTitle={<Trash color="white" />}
+            title="Apagar Game"
+            description="Você tem certeza de que deseja apagar esse game?"
+            className="size-12 rounded-full bg-red-600 dark:bg-red-600 hover:bg-red-700"
+            buttonActionTitle="Apagar"
+            buttonSubmitClassname="w-full text-white bg-red-600 hover:bg-red-700 md:w-fit"
+            onClose={() => console.log("I'm closing")}
+          >
+            <UnlinkMessage>
+              <p className="max-w-[50rem] text-[1rem] md:text-[1.2rem] lg:text-[1.4rem]">
+                Você tem certeza de que deseja apagar o game{' '}
+                <strong>{row.original?.nome || 'Game'} </strong> ? Todas as
+                incrições cadastradas nesse game serão apagadas
+              </p>
+            </UnlinkMessage>
+          </Dialog>
+        </div>
+      );
+    },
+  },
+];
