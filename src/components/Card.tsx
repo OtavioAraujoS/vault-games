841e49c3e8edf9ce7b07b7d0c34955bd706f2c01
otavioaraujo08 <otavioaraujo490@gmail.com>
2025-01-09 09:27:49 -0300

    feat: add Card component for displaying title and value

---
+++ src/components/Card.tsx
@@ -0,0 +1,28 @@
+import {
+  Card as ShadcnCard,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+
+interface CardProps {
+  title: string;
+  value: string | number;
+}
+
+export const Card = ({ title, value }: CardProps) => {
+  return (
+    <ShadcnCard className="dark:bg-[#212121] rounded border border-gray-500 shadow-md w-1/5 min-w-[12rem] m-4">
+      <CardHeader>
+        <CardTitle className="text-[#7E7E7E] font-sans font-semibold tracking-wide text-[1rem] md:text-[1.1rem] lg:text-[1.2rem]">
+          {title}
+        </CardTitle>
+      </CardHeader>
+      <CardContent>
+        <p className="dark:text-white text-[2.5rem] font-bebas tracking-widest">
+          {value}
+        </p>
+      </CardContent>
+    </ShadcnCard>
+  );
+};
