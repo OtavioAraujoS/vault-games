a53c37ccfededfb3acc76fa6ba09f745e2201fab
otavioaraujo08 <otavioaraujo490@gmail.com>
2025-01-09 09:29:42 -0300

    feat: add Jogos page with Card components for displaying game statistics

--- src/pages/jogos/Jogos.tsx
+++ src/pages/jogos/Jogos.tsx
@@ -1,7 +1,14 @@
+import { Card } from '@/components/Card';
+
 export const Jogos = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-full">
-      <h1 className="text-3xl font-bold">Jogos</h1>
+    <div className="flex flex-col h-full min-h-[100vh] w-full">
+      <div className="flex flex-wrap gap-4">
+        <Card title="Jogos Cadastrados" value="10" />
+        <Card title="Jogos Não Iniciados" value="10" />
+        <Card title="Jogos Pausados" value="10" />
+        <Card title="Jogos Completos" value="10" />
+      </div>
     </div>
   );
 };
