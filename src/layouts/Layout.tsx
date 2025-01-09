2d0d16e1f1c87756ee30b9a7d9bd07aeb79466d3
otavioaraujo08 <otavioaraujo490@gmail.com>
2025-01-09 11:25:27 -0300

    feat: update header background color for improved visibility in dark mode

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -11,7 +11,7 @@ const Layout: React.FC = () => {
       <SidebarProvider>
         <Sidebar />
         <div className="h-full w-full dark:bg-[#181818]">
-          <header className="bg-white dark:bg-[#212121] bg-gray border-b border-gray-400 text-black dark:text-white p-4 flex justify-between items-center">
+          <header className="bg-[#FAFAFA] dark:bg-[#212121] bg-gray border-b border-gray-400 text-black dark:text-white p-4 flex justify-between items-center">
             <SidebarTrigger className="size-10 dark:bg-[#212121] border border-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-500" />
             <ThemeToggle />
           </header>
