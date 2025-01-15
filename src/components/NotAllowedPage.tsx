69819906da5ce11953380ff72083f8b6cd747785
otavioaraujo08 <otavioaraujo490@gmail.com>
2025-01-15 15:19:09 -0300

    feat: refactor NotAllowedPage to use LoginContext and improve user redirection

--- src/components/NotAllowedPage.tsx
+++ src/components/NotAllowedPage.tsx
@@ -1,25 +1,36 @@
-import { AuthContext } from '@/context/AuthContext';
+import { LoginContext } from '@/context/LoginContext';
 import { Angry } from 'lucide-react';
-import { useContext } from 'react';
 import { useNavigate } from 'react-router';
 import { Button } from './ui/button';
 
 export const NotAllowedPage = () => {
-  const authContext = useContext(AuthContext);
+  const { loginInfos, logout } = LoginContext();
   const navigate = useNavigate();
 
-  if (!authContext) {
-    return <div>Loading...</div>;
-  }
-
-  const { logout } = authContext;
-
-  const LogOut = () => {
+  const handleLogOut = () => {
     logout();
     navigate('/');
   };
 
-  return (
+  const handleRedirectUser = () => {
+    navigate('/dashboard');
+  };
+
+  return loginInfos ? (
+    <div className="bg-[#dad8d8] flex flex-col justify-center items-center gap-4 w-full h-full min-h-[100vh]">
+      <Angry className="text-red-600 w-fit h-[6rem] md:h-[8rem] lg:h-[10rem]" />
+      <h1 className="text-[1.5rem] font-bold font-bebas text-center max-w-[60%] sm:text-[2rem] md:text-[2.5rem] lg:text-[3rem]">
+        Página não encontrada! Clique no botão abaixo para ser redirecionado à
+        página de Usuário
+      </h1>
+      <Button
+        className="w-fit p-2 min-w-[8rem] md:min-w-[10rem] lg:min-w-[15rem] bg-red-600 hover:bg-red-700"
+        onClick={handleRedirectUser}
+      >
+        Página de Usuário
+      </Button>
+    </div>
+  ) : (
     <div className="bg-[#dad8d8] flex flex-col justify-center items-center gap-4 w-full h-full min-h-[100vh]">
       <Angry className="text-red-600 w-fit h-[6rem] md:h-[8rem] lg:h-[10rem]" />
       <h1 className="text-[1.5rem] font-bold font-bebas text-center max-w-[60%] sm:text-[2rem] md:text-[2.5rem] lg:text-[3rem]">
@@ -28,7 +39,7 @@ export const NotAllowedPage = () => {
       </h1>
       <Button
         className="w-fit p-2 min-w-[8rem] md:min-w-[10rem] lg:min-w-[15rem] bg-red-600 hover:bg-red-700"
-        onClick={LogOut}
+        onClick={handleLogOut}
       >
         Realizar Login
       </Button>
