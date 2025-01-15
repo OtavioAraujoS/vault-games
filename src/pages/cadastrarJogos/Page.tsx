72d45b55a63f6ebef051c54e8e334b993efd4a8e
otavioaraujo08 <otavioaraujo490@gmail.com>
2025-01-15 16:12:41 -0300

    feat: create CadastrarJogos component for game registration page

---
+++ src/pages/cadastrarJogos/Page.tsx
@@ -0,0 +1,12 @@
+export const CadastrarJogos = () => {
+  return (
+    <div className="grid grid-cols-2 gap-2 p-10 min-h-[100vh] w-full dark:bg-[#2B2738] dark:text-white">
+      <div>Imagem Aleatoria</div>
+      <div className="flex items-center justify-center">
+        <h1 className="text-[4rem] font-bold font-bebas tracking-wider text-center">
+          Cadastrar Jogo
+        </h1>
+      </div>
+    </div>
+  );
+};
