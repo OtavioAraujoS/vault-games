53e2cb7d47b30588050eeb0c59f4c25c93b52954
otavioaraujo08 <otavioaraujo490@gmail.com>
2025-01-16 14:43:32 -0300

    feat: enhance Login and LoginForm components with improved styling and accessibility

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -6,8 +6,11 @@ export const Login = () => {
   return (
     <div className="grid min-h-svh lg:grid-cols-2">
       <div className="flex flex-col gap-4 p-6 md:p-10">
-        <div className="flex justify-center gap-2 md:justify-start">
-          <a href="#" className="flex items-center gap-2 font-medium">
+        <div className="flex justify-center gap-2 md:justify-start dark:text-black">
+          <a
+            href="#"
+            className="flex items-center gap-2 font-medium dark:text-black"
+          >
             <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
               <GalleryVerticalEnd className="size-4" />
             </div>
@@ -24,7 +27,7 @@ export const Login = () => {
         <img
           src="/loginWallpaper.png"
           alt="Image"
-          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+          className="absolute inset-0 h-full w-full object-cover"
         />
       </div>
     </div>
--- src/pages/auth/LoginForm.tsx
+++ src/pages/auth/LoginForm.tsx
@@ -1,16 +1,16 @@
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
+import { LoginContext } from '@/context/LoginContext';
 import { useToast } from '@/hooks/use-toast';
 import { UsersInfo } from '@/types/User';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useState } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router';
+import { z } from 'zod';
 import { userService } from '../../services/user';
 import { mapError } from '../../utils/ErrosMap';
 import { loginSchemma } from './formSchemma';
-import { LoginContext } from '@/context/LoginContext';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useState } from 'react';
-import { z } from 'zod';
 
 export const LoginForm = () => {
   const navigate = useNavigate();
@@ -58,16 +58,16 @@ export const LoginForm = () => {
     <form onSubmit={handleSubmit(handleLogin)}>
       <div className="flex justify-center items-center">
         <div className="flex flex-col items-center justify-center p-10 gap-3 md:min-h-[30rem] lg:min-h-[35rem] min-w-full sm:min-w-[20rem] md:min-w-[25rem] lg:min-w-[30rem] xl:min-w-[35rem]">
-          <h1 className="text-[1.5rem] md:text-[2rem] lg:text-[3rem] font-medium text-black font-bebas tracking-wide mb-4 dark:text-white">
+          <h1 className="text-[1.5rem] md:text-[2rem] lg:text-[3rem] font-medium text-black font-bebas tracking-wide mb-4">
             {isLogin ? 'Login' : 'Registrar'}
           </h1>
           <div className="flex flex-col gap-1 w-full">
-            <label className="text-black text-[0.9rem] md:text-[1rem] lg:text-[1.2rem] font-bold font-sans dark:text-white">
+            <label className="text-black text-[0.9rem] md:text-[1rem] lg:text-[1.2rem] font-bold font-sans">
               Usuário
             </label>
             <Input
               placeholder="Usuário"
-              className="mb-4 p-2 border dark:border-white rounded w-full"
+              className="mb-4 p-2 border rounded w-full"
               {...register('nome')}
             />
             <p className="text-red-500 text-[0.8rem]">
@@ -80,13 +80,13 @@ export const LoginForm = () => {
           </div>
 
           <div className="flex flex-col gap-1 w-full">
-            <label className="text-black text-[0.9rem] md:text-[1rem] lg:text-[1.2rem] font-bold font-sans dark:text-white">
+            <label className="text-black text-[0.9rem] md:text-[1rem] lg:text-[1.2rem] font-bold font-sans">
               Senha
             </label>
             <Input
               type="password"
               placeholder="Senha"
-              className="mb-4 p-2 border dark:border-white rounded w-full"
+              className="mb-4 p-2 border rounded w-full"
               {...register('password')}
             />
             <p className="text-red-500 text-[0.8rem]">
@@ -101,7 +101,7 @@ export const LoginForm = () => {
           <div className="w-full text-center">
             <Button
               type="submit"
-              className="bg-blue-700 text-white p-2 rounded w-full hover:bg-blue-800"
+              className="bg-blue-700 text-white p-2 rounded w-full hover:bg-blue-800 dark:bg-blue-500 dark:hover:bg-blue-600 dark:text-white"
               disabled={loading}
             >
               {loading ? 'Carregando' : isLogin ? 'Entrar' : 'Registrar'}
