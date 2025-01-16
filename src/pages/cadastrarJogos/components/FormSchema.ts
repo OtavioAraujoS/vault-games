86850d29258d18a3d080cb7deafd7f975757094f
otavioaraujo08 <otavioaraujo490@gmail.com>
2025-01-16 16:08:37 -0300

    feat: add validation schema for game registration using Zod

---
+++ src/pages/cadastrarJogos/components/FormSchema.ts
@@ -0,0 +1,20 @@
+import { z } from 'zod';
+
+export const registerGameSchema = z.object({
+  nome: z
+    .string({ required_error: 'Nome é obrigatório' })
+    .nonempty('Nome é obrigatório'),
+  image: z
+    .string({ required_error: 'Imagem é obrigatória' })
+    .nonempty('Imagem é obrigatória'),
+  description: z
+    .string({ required_error: 'Descrição é obrigatória' })
+    .nonempty('Descrição é obrigatória'),
+  status: z.enum(['Pendente', 'Progresso', 'Pausado', 'Completo'], {
+    required_error: 'Status é obrigatório',
+  }),
+  hours: z.number({ required_error: 'Horas é obrigatório' }),
+  review: z
+    .string({ required_error: 'Review é obrigatório' })
+    .nonempty('Review é obrigatório'),
+});
