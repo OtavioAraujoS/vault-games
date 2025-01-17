5ec8bc8371c683714e50ccf821156f85fed88a95
otavioaraujo08 <otavioaraujo490@gmail.com>
2025-01-17 15:52:49 -0300

    feat: enhance EditarJogos component with game details form and navigation

--- src/pages/editarJogos/Page.tsx
+++ src/pages/editarJogos/Page.tsx
@@ -1,12 +1,25 @@
+import { Button } from '@/components/ui/button';
 import { useToast } from '@/hooks/use-toast';
 import { gameService } from '@/services/games';
 import { Game } from '@/types/Games';
 import { mapError } from '@/utils/ErrosMap';
+import { Undo2 } from 'lucide-react';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
+import { Form } from './compoents/Form';
 
 export const EditarJogos = () => {
-  const [gameInfos, setGameInfos] = useState<Game | object>({});
+  const [gameInfos, setGameInfos] = useState<Game>({
+    _id: '',
+    nome: '',
+    description: '',
+    image: '',
+    hours: 0,
+    createdAt: '',
+    status: 'Pendente',
+    userId: '',
+    review: '',
+  });
   const { id } = useParams();
   const { toast } = useToast();
 
@@ -31,12 +44,40 @@ export const EditarJogos = () => {
     getGameInfos();
   }, [id]);
 
-  console.log(gameInfos);
-
-  return (
-    <div className="w-full h-full min-h-[100vh] dark:text-white">
-      <h1>Editar Jogos</h1>
-      <p>Id do jogo: {id}</p>
+  return id && gameInfos.nome ? (
+    <div className="grid min-h-svh lg:grid-cols-2">
+      <div className="flex flex-col gap-12 p-6 md:p-10 dark:bg-[#181818]">
+        <div className="flex justify-center gap-2 md:justify-start">
+          <a
+            href="/jogos"
+            className="flex items-center gap-2 font-medium dark:text-white"
+          >
+            <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
+              <Undo2 className="size-4 dark:text-white" />
+            </div>
+            Retornar
+          </a>
+        </div>
+        <div className="flex w-full">
+          <Form gameId={id} game={gameInfos} />
+        </div>
+      </div>
+      <div className="relative hidden bg-muted lg:block">
+        <img
+          src="/updateFormWallpaper.webp"
+          alt="Image"
+          className="absolute inset-0 h-full w-full object-cover"
+        />
+      </div>
+    </div>
+  ) : (
+    <div className="flex flex-col gap-4 justify-center items-center h-full min-h-[100vh] dark:bg-[#181818]">
+      <h1 className="text-2xl font-bold dark:text-white">
+        Jogo não encontrado
+      </h1>
+      <Button className="w-1/4">
+        <a href="/jogos">Retornar para aba de jogos</a>
+      </Button>
     </div>
   );
 };
