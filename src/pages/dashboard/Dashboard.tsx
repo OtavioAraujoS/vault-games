048da51dfa99b1525ca53691922ed4150b014a1f
otavioaraujo08 <otavioaraujo490@gmail.com>
2025-01-17 16:05:16 -0300

    feat: fetch dashboard information on component mount and handle errors

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,19 +1,49 @@
 import { NotAllowedPage } from '@/components/NotAllowedPage';
+import { TitlePage } from '@/components/TitlePage';
 import { LoginContext } from '@/context/LoginContext';
+import { useToast } from '@/hooks/use-toast';
+import { dashboardService } from '@/services/dashboard';
+import { mapError } from '@/utils/ErrosMap';
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router';
 
 export const Dashboard = () => {
-  const navigate = useNavigate();
   const { loginInfos, logout } = LoginContext();
+  const navigate = useNavigate();
+  const { toast } = useToast();
 
   const LogOut = () => {
     logout();
     navigate('/');
   };
 
+  useEffect(() => {
+    const handleGetDashboardInfos = async () => {
+      try {
+        if (!loginInfos) {
+          return;
+        }
+
+        const response = await dashboardService.getDashboardInfos();
+        console.log(response);
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      } catch (error: any) {
+        toast({
+          title: 'Erro ao buscar informações do dashboard',
+          description:
+            mapError(error) ||
+            'Não foi possível buscar as informações do dashboard',
+          variant: 'destructive',
+        });
+      }
+    };
+
+    handleGetDashboardInfos();
+  }, []);
+
   return loginInfos ? (
     <div style={{ padding: '20px', minHeight: '100vh' }}>
-      <h1 className="dark:text-white">Dashboard</h1>
+      <TitlePage title="Dashboard" />
       <p className="dark:text-white">Bem-vindo ao painel de controle!</p>
       <div style={{ marginTop: '20px' }}>
         <h2>Estatísticas</h2>
