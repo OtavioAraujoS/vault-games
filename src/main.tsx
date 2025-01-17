5dbad238356369ff3fe190f536e970d7618296e8
otavioaraujo08 <otavioaraujo490@gmail.com>
2025-01-17 15:09:53 -0300

    improvement: wrap App component with ThemeProvider for theme management

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,14 @@ import { Toaster } from '@/components/ui/toaster';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
+import { ThemeProvider } from './context/ThemeContext.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-    <Toaster />
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <App />
+      <Toaster />
+    </ThemeProvider>
   </StrictMode>
 );
