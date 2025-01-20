3b98bde10b6c39b832aac960c10ef62e452d3766
otavioaraujo08 <otavioaraujo490@gmail.com>
2025-01-20 15:49:49 -0300

    feat: enhance Dashboard component with user greeting and game status distribution

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,21 +1,16 @@
 import { NotAllowedPage } from '@/components/NotAllowedPage';
-import { TitlePage } from '@/components/TitlePage';
 import { LoginContext } from '@/context/LoginContext';
 import { useToast } from '@/hooks/use-toast';
 import { dashboardService } from '@/services/dashboard';
+import { DashboardInfos } from '@/types/Dashboard';
 import { mapError } from '@/utils/ErrosMap';
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router';
+import { useEffect, useState } from 'react';
+import { GamesByStatus } from './components/GamesByStatus';
 
 export const Dashboard = () => {
-  const { loginInfos, logout } = LoginContext();
-  const navigate = useNavigate();
+  const [dashboardInfos, setDashboardInfos] = useState<DashboardInfos>();
+  const { loginInfos } = LoginContext();
   const { toast } = useToast();
-
-  const LogOut = () => {
-    logout();
-    navigate('/');
-  };
 
   useEffect(() => {
     const handleGetDashboardInfos = async () => {
@@ -26,6 +21,7 @@ export const Dashboard = () => {
 
         const response = await dashboardService.getDashboardInfos();
         console.log(response);
+        setDashboardInfos(response);
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
       } catch (error: any) {
         toast({
@@ -42,18 +38,44 @@ export const Dashboard = () => {
   }, []);
 
   return loginInfos ? (
-    <div style={{ padding: '20px', minHeight: '100vh' }}>
-      <TitlePage title="Dashboard" />
-      <p className="dark:text-white">Bem-vindo ao painel de controle!</p>
-      <div style={{ marginTop: '20px' }}>
-        <h2>Estatísticas</h2>
-        <ul className="dark:text-white">
-          <li>ID: {loginInfos?.id}</li>
-          <li>Usuário: {loginInfos?.name}</li>
-          <li>
-            <button onClick={LogOut}>LogOut</button>
-          </li>
-        </ul>
+    <div className="flex flex-col gap-8 dark:text-white min-h-[100vh] p-12">
+      <div className="flex flex-wrap-reverse justify-center items-center gap-6 pb-4 lg:justify-between lg:gap-0">
+        <div>
+          <h1 className="font-bold font-bebas text-[2.5rem] text-center lg:text-left">
+            Seja bem vindo(a), {loginInfos.name}
+          </h1>
+          <p className="text-gray-500 dark:text-gray-400 text-center lg:text-left">
+            Aqui você pode ver algumas informações sobre o seu perfil e sobre o
+            sistema.
+          </p>
+        </div>
+
+        <div>
+          <img
+            src={
+              loginInfos.image ||
+              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSt2deTTpL5Z_Y-FBxr3DhfCdoDNHvUEmtvjQ&s'
+            }
+            alt="profile"
+            className="rounded-full size-28 shadow-lg"
+          />
+        </div>
+      </div>
+
+      <div className="flex flex-col text-center gap-6 w-full border-y-2 border-gray-300 dark:border-gray-700 py-6 lg:text-left">
+        <h2 className="font-bold font-bebas text-[2rem] text-center lg:text-left">
+          Informações de status de jogos no sistema
+        </h2>
+
+        <GamesByStatus
+          gameStatusDistribution={dashboardInfos?.gameStatusDistribution}
+        />
+      </div>
+
+      <div className="flex flex-col">
+        <h2 className="font-bold font-bebas text-[2rem] text-center lg:text-left">
+          Jogos cadastrados por usuário
+        </h2>
       </div>
     </div>
   ) : (
