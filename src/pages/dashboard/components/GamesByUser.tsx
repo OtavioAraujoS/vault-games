d9f7f4a315e796b09d4f953d0e5dc99301aa4fa9
otavioaraujo08 <otavioaraujo490@gmail.com>
2025-01-21 15:16:14 -0300

    feat: add GamesByUser component to display games associated with a user

---
+++ src/pages/dashboard/components/GamesByUser.tsx
@@ -0,0 +1,56 @@
+import { StatusChip } from '@/components/StatusChip';
+import { useToast } from '@/hooks/use-toast';
+import { gameService } from '@/services/games';
+import { Game } from '@/types/Games';
+import { useEffect, useState } from 'react';
+
+interface GamesByUserProps {
+  userId: string;
+}
+
+export const GamesByUser = ({ userId }: GamesByUserProps) => {
+  const [gamesByUser, setGamesByUser] = useState<Game[] | []>([]);
+  const { toast } = useToast();
+
+  useEffect(() => {
+    const getGamesByUser = async () => {
+      try {
+        if (!userId) {
+          return;
+        }
+
+        const response = await gameService.getGamesByUser(userId);
+        setGamesByUser(response);
+      } catch {
+        toast({
+          title: 'Erro ao buscar informações do dashboard',
+          description: 'Não foi possível buscar as informações do dashboard',
+        });
+      }
+    };
+
+    getGamesByUser();
+  }, []);
+
+  return gamesByUser ? (
+    <div className="flex flex-wrap gap-6 mt-5">
+      {gamesByUser.map((item: Game) => (
+        <div
+          className="flex flex-col gap-2 h-fit min-h-[25rem] w-full max-w-64 max-h-[26rem] overflow-hidden bg-transparent border border-gray-300 rounded-md p-4"
+          key={item._id}
+        >
+          <img src={item.image} alt={item.nome} className="size-48" />
+          <h1 className="text-[1.8rem] font-bebas tracking-wider">
+            {item.nome}
+          </h1>
+          <h2 className="text-[0.8rem] text-wrap text-ellipsis whitespace-nowrap h-[3.5rem] truncate ...">
+            {item.description}
+          </h2>
+          <div className="mt-4">
+            <StatusChip status={item.status} />
+          </div>
+        </div>
+      ))}
+    </div>
+  ) : null;
+};
