4151bbce79c3e5c5be2dc6e83ccc60a26dbadbfc
otavioaraujo08 <otavioaraujo490@gmail.com>
2025-01-23 15:27:55 -0300

    fix: update route to use EditarUsuario component for user editing

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { CadastrarJogos } from './pages/jogos/cadastrarJogos/Page';
 import { EditarJogos } from './pages/jogos/editarJogos/Page';
 import { Jogos } from './pages/jogos/Page';
 import { CadastrarUsuario } from './pages/usuarios/cadastrarUsuario/Page';
+import { EditarUsuario } from './pages/usuarios/editarUsuario/Page';
 import { Usuarios } from './pages/usuarios/Page';
 
 function App() {
@@ -20,7 +21,7 @@ function App() {
           <Route path="usuarios" element={<Usuarios />} />
         </Route>
         <Route path="jogos/:id" element={<EditarJogos />} />
-        <Route path="usuarios/:id" element={<EditarJogos />} />
+        <Route path="usuarios/:id" element={<EditarUsuario />} />
         <Route path="cadastrar-jogos" element={<CadastrarJogos />} />
         <Route path="cadastrar-usuario" element={<CadastrarUsuario />} />
         <Route path="*" element={<NotAllowedPage />} />
