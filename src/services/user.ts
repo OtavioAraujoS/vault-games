7e32528d091297f3c23a8cb9b1c110eeb15e02bd
otavioaraujo08 <otavioaraujo490@gmail.com>
2025-01-23 15:28:15 -0300

    feat: add methods to get user by ID and update user information

--- src/services/user.ts
+++ src/services/user.ts
@@ -2,11 +2,16 @@ import { UserLogin, UsersInfo } from '../types/User';
 import { api as apiService, ApiService, defaultUrl } from './api';
 
 class UserService {
-  constructor(private readonly api: ApiService) {}
+  constructor(private readonly api: ApiService) { }
 
   public getAllUsers = async (): Promise<UsersInfo[]> => {
     const response = await this.api.get(`${defaultUrl}/user/all`);
     return response as UsersInfo[];
+  };
+
+  public getUserById = async (id: string): Promise<UsersInfo> => {
+    const response = await this.api.get(`${defaultUrl}/user/${id}`);
+    return response as UsersInfo;
   };
 
   public login = async (data: UserLogin): Promise<UsersInfo> => {
@@ -22,5 +27,12 @@ class UserService {
     });
     return response as UsersInfo;
   };
+
+  public updateUserInfos = async (id: string, data: Partial<UsersInfo>): Promise<UsersInfo> => {
+    const response = await this.api.put(`${defaultUrl}/user/update-infos/${id}`, {
+      ...data,
+    });
+    return response as UsersInfo;
+  }
 }
 export const userService = new UserService(apiService);
