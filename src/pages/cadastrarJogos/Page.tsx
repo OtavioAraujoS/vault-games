6d8dc3e03618454dc84d8763da607fded7488782
otavioaraujo08 <otavioaraujo490@gmail.com>
2025-01-23 14:08:15 -0300

    feat: replace anchor tag with Link component for navigation in CadastrarJogos page

--- src/pages/cadastrarJogos/Page.tsx
+++ src/pages/cadastrarJogos/Page.tsx
@@ -1,4 +1,5 @@
 import { Undo2 } from 'lucide-react';
+import { Link } from 'react-router';
 import { Form } from './components/Form';
 
 export const CadastrarJogos = () => {
@@ -6,15 +7,15 @@ export const CadastrarJogos = () => {
     <div className="grid min-h-svh lg:grid-cols-2">
       <div className="flex flex-col gap-12 p-6 md:p-10 dark:bg-[#181818]">
         <div className="flex justify-center gap-2 md:justify-start">
-          <a
-            href="/jogos"
+          <Link
+            to="/jogos"
             className="flex items-center gap-2 font-medium dark:text-white"
           >
             <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
               <Undo2 className="size-4 dark:text-white" />
             </div>
             Retornar
-          </a>
+          </Link>
         </div>
         <div className="flex w-full">
           <Form />
