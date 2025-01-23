5ac26dc66910bf729c8e069cc6930db927076f36
otavioaraujo08 <otavioaraujo490@gmail.com>
2025-01-23 14:08:07 -0300

    feat: implement user fetching and display in Usuarios page with error handling

--- src/pages/usuarios/Page.tsx
+++ src/pages/usuarios/Page.tsx
@@ -1,9 +1,61 @@
 import { TitlePage } from "@/components/TitlePage"
+import { Button } from "@/components/ui/button"
+import { useToast } from "@/hooks/use-toast"
+import { userService } from "@/services/user"
+import { UsersInfo } from "@/types/User"
+import { Plus } from "lucide-react"
+import { useEffect, useState } from "react"
+import { Link } from "react-router"
 
 export const Usuarios = () => {
+    const [users, setUsers] = useState<UsersInfo[]>([])
+    const { toast } = useToast()
+
+    useEffect(() => {
+        const getSystemUsers = async () => {
+            try {
+                const response = await userService.getAllUsers()
+                setUsers(response)
+            } catch {
+                toast({
+                    title: "Erro ao buscar usuários",
+                    description: "Ocorreu um erro ao buscar os usuários do sistema",
+                    variant: "destructive"
+                })
+            }
+        }
+
+        getSystemUsers()
+    }, [])
     return (
-        <div className="h-full min-h-screen p-12 gap-4">
-            <TitlePage title="Usuários" />
+        <div className="flex flex-col h-full min-h-screen p-12 gap-8">
+            <div className="flex flex-wrap justify-between items-center">
+                <TitlePage title="Usuários Cadastrados no Sistema" />
+                <Link to="/usuarios/cadastrar">
+                    <Button className="text-[0.6rem] md:text-[0.8rem] lg:text-[1rem] h-10 bg-blue-600 text-white dark:text-white dark:bg-blue-600 dark:hover:bg-blue-700 hover:bg-blue-700"><Plus /> Cadastrar Usuário</Button>
+                </Link>
+            </div>
+
+            <div className="flex flex-wrap gap-4">
+                {
+                    users.length > 0 ? users.map((user: UsersInfo) => (
+                        <div className="flex flex-col gap-6 p-8 justify-center items-center bg-[#FAFAFA] dark:bg-[#212121] rounded border border-gray-300 dark:border-white" key={user._id}>
+                            <img src={user.picture !== "userPicture" ?
+                                user.picture :
+                                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSt2deTTpL5Z_Y-FBxr3DhfCdoDNHvUEmtvjQ&s'
+                            } alt={user.nome} className="size-14 md:size-20 lg:size-24 rounded-full" />
+                            <div className="flex flex-col gap-1 items-center text-center">
+                                <h2 className="lg:text-[1.4rem] tracking-wider font-bebas dark:text-white">{user.nome}</h2>
+                                <p className="text-gray-600 dark:text-gray-300 lg:text-[1.2rem] font-bebas">Usuário desde: {new Date(user.createdAt).toLocaleDateString()}</p>
+                            </div>
+                        </div>
+
+                    ))
+                        : (
+                            <p>Nenhum usuário cadastrado</p>
+                        )
+                }
+            </div>
         </div>
     )
 }
