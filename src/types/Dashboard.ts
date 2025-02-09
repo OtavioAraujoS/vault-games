91e35680947d72e9baf00bf60d0417f3bf3d82b3
Otávio Araujo <51797012+otavioaraujo08@users.noreply.github.com>
2025-02-09 01:12:28 -0300

    feat: add ranked users by registered games to Dashboard interface

--- src/types/Dashboard.ts
+++ src/types/Dashboard.ts
@@ -13,9 +13,17 @@ export interface GameStatusDistribution {
   count: number;
 }
 
+export interface RankedUsersByRegisteredGames {
+  gameCount: number;
+  userId: string;
+  userName: string;
+  userImage: string;
+}
+
 export interface DashboardInfos {
   totalUsers: number;
   totalGames: number;
   gamesPerUser: GamesPerUser[];
   gameStatusDistribution: GameStatusDistribution[];
+  rankedUsersByRegisteredGames: RankedUsersByRegisteredGames[];
 }
