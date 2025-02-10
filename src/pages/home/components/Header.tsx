f636ff1c484d55805a62824a18260a092947bb91
otavio.silva <otavioaraujo490@gmail.com>
2025-02-10 16:16:09 -0300

    feat: improve Header component styling for better alignment and visibility

--- src/pages/home/components/Header.tsx
+++ src/pages/home/components/Header.tsx
@@ -17,8 +17,8 @@ export const Header = ({
 
   return (
     <header className="w-[80%] min-h-[8vh] flex justify-between items-center p-4 mt-4">
-      <div className="flex items-center">
-        <h1 className="font-bebas tracking-wider text-xl text-zinc-900 lg:text-2xl">
+      <div className="flex items-center mx-auto lg:mx-0">
+        <h1 className="font-bebas tracking-wider text-zinc-900 text-3xl">
           Vault Games
         </h1>
       </div>
@@ -46,7 +46,7 @@ export const Header = ({
         </ul>
       </div>
 
-      <div>
+      <div className="hidden lg:block">
         <button
           className="w-[7rem] h-[2rem] lg:w-[8rem] lg:h-[2.5rem] bg-black text-white text-[0.8rem] lg:text-[1rem] rounded-3xl"
           onClick={redirectToLogin}
