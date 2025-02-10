608d3a50501c26a7e57367d5ba553194df9e240d
otavio.silva <otavioaraujo490@gmail.com>
2025-02-10 16:20:14 -0300

    feat: add navigation functionality to Login component for redirecting to Home

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,12 +1,20 @@
 import { GalleryVerticalEnd } from 'lucide-react';
+import { useNavigate } from 'react-router';
 import './index.css';
 import { LoginForm } from './LoginForm';
 
 export const Login = () => {
+  const redirect = useNavigate();
+  const redirectToHome = () => {
+    redirect('/');
+  };
   return (
     <div className="grid min-h-svh lg:grid-cols-2">
       <div className="flex flex-col gap-4 p-6 md:p-10">
-        <div className="flex justify-center gap-2 md:justify-start dark:text-black">
+        <div
+          className="flex justify-center gap-2 cursor-pointer md:justify-start dark:text-black"
+          onClick={redirectToHome}
+        >
           <a className="flex items-center gap-2 font-medium dark:text-black">
             <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
               <GalleryVerticalEnd className="size-4" />
@@ -25,7 +33,7 @@ export const Login = () => {
           src="/loginWallpaper.webp"
           alt="Image"
           className="absolute inset-0 h-full w-full object-cover"
-          loading='lazy'
+          loading="lazy"
         />
       </div>
     </div>
