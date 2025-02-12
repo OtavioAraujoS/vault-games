ae2c30da2d20b312d6be1947021e0ae2aecf1393
otavio.silva <otavioaraujo490@gmail.com>
2025-02-12 09:21:17 -0300

    feat: add Features component to showcase tools and game management

---
+++ src/pages/home/components/Features.tsx
@@ -0,0 +1,16 @@
+export const Features = () => {
+  return (
+    <div className="bg-black h-full min-h-screen flex flex-col justify-center items-center px-4 lg:p-0">
+      <div className="flex flex-col items-center gap-4">
+        <h1 className="font-oswald text-white tracking-wider text-4xl sm:text-5xl md:text-6xl lg:text-7xl text-center">
+          Explore nossas ferramentas
+        </h1>
+        <p className="text-zinc-400 text-center max-w-[45rem] text-base md:text-lg lg:text-xl">
+          <span className="text-white">Gerencie suas coleções</span> com
+          facilidade, adicione jogos à sua lista de desejos e veja quais amigos
+          possuem os mesmos jogos que você.
+        </p>
+      </div>
+    </div>
+  );
+};
