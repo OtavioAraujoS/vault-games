2e0e704dc11b32c31a978e0b38a48399d3816b41
otavio.silva <otavioaraujo490@gmail.com>
2025-02-26 15:43:22 -0300

    feat: enhance error mapping to include missing required field and improve matching logic

--- src/utils/ErrosMap.ts
+++ src/utils/ErrosMap.ts
@@ -5,8 +5,19 @@ type ErrorMap = {
 const errorMessages: ErrorMap = {
   'User not found or incorrect password':
     'Usuário não encontrado ou senha incorreta.',
+  'Missing required field': 'Campo obrigatório não preenchido.',
 };
 
 export function mapError(errorCode: string): string {
-  return errorMessages[errorCode] || 'Ocorreu um erro desconhecido.';
+  const lowerCaseErrorCode = errorCode.toLowerCase();
+  for (const key of Object.keys(errorMessages)) {
+    if (lowerCaseErrorCode.includes(key.toLowerCase())) {
+      console.log(`Match found: ${key}`);
+      const errorMessage = errorMessages[key];
+      if (typeof errorMessage === 'string') {
+        return errorMessage;
+      }
+    }
+  }
+  return 'Erro desconhecido';
 }
