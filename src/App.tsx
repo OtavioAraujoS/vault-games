722946adb6b4b9115edcdc1ac2f3f3051172f66e
otavio.silva <otavioaraujo490@gmail.com>
2025-03-26 09:26:07 -0300

    feat: add route for user profile page in App component

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import { CadastrarJogos } from './pages/jogos/cadastrarJogos/Page';
 import { EditarJogos } from './pages/jogos/editarJogos/Page';
 import { Jogos } from './pages/jogos/Page';
 import { JogosPorStatus } from './pages/jogosPorStatus/Page';
+import Profile from './pages/profile/Page';
 import { CadastrarUsuario } from './pages/usuarios/cadastrarUsuario/Page';
 import { EditarUsuario } from './pages/usuarios/editarUsuario/Page';
 import { Usuarios } from './pages/usuarios/Page';
@@ -23,6 +24,7 @@ function App() {
           <Route path="jogos" element={<Jogos />} />
           <Route path="jogos-por-status" element={<JogosPorStatus />} />
           <Route path="usuarios" element={<Usuarios />} />
+          <Route path="profile/:id" element={<Profile />} />
         </Route>
         <Route path="jogos/:id" element={<EditarJogos />} />
         <Route path="usuarios/:id" element={<EditarUsuario />} />
