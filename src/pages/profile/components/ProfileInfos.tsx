df6cc9d1adaff3abee6853e49b08474c8f917402
otavio.silva <otavioaraujo490@gmail.com>
2025-03-26 09:33:01 -0300

    feat: add user ID to ProfileInfos component for profile editing navigation

--- src/pages/profile/components/ProfileInfos.tsx
+++ src/pages/profile/components/ProfileInfos.tsx
@@ -1,12 +1,23 @@
 import { Button } from '@/components/ui/button';
 import { Pencil } from 'lucide-react';
+import { useNavigate } from 'react-router';
 
 export interface ProfileInfosProps {
   picture: string;
   name: string;
+  userId: string;
 }
 
-export default function ProfileInfos({ name, picture }: ProfileInfosProps) {
+export default function ProfileInfos({
+  name,
+  picture,
+  userId,
+}: ProfileInfosProps) {
+  const router = useNavigate();
+
+  const handleEditProfile = () => {
+    router(`/usuarios/${userId}`);
+  };
   return (
     <div className="flex justify-between items-center gap-4 p-9 rounded-lg border-2 border-lime-600 bg-[#FAFAFA] dark:bg-[#212121]">
       <div className="flex items-center gap-8">
@@ -21,7 +32,10 @@ export default function ProfileInfos({ name, picture }: ProfileInfosProps) {
           </h2>
         </div>
       </div>
-      <Button className="w-fit h-10 p-4 rounded-2xl bg-lime-600 hover:bg-lime-700 text-white dark:text-zinc-900 border-2 border-lime-600 dark:border-zinc-900">
+      <Button
+        className="w-fit h-10 p-4 rounded-2xl bg-lime-600 hover:bg-lime-700 text-white dark:text-zinc-900 border-2 border-lime-600 dark:border-zinc-900"
+        onClick={handleEditProfile}
+      >
         Editar Perfil <Pencil />
       </Button>
     </div>
