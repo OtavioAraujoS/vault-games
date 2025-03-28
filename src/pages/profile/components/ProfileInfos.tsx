b264a69ec39a9858a065069a8e0964274a5b886b
otavio.silva <otavioaraujo490@gmail.com>
2025-03-28 09:09:54 -0300

    feat: add password visibility toggle to ProfileInfos component

--- src/pages/profile/components/ProfileInfos.tsx
+++ src/pages/profile/components/ProfileInfos.tsx
@@ -1,25 +1,34 @@
 'use client';
 import { Button } from '@/components/ui/button';
 import { LoginContext } from '@/context/LoginContext';
-import { Pencil } from 'lucide-react';
+import { Eye, EyeOff, Pencil } from 'lucide-react';
+import { useState } from 'react';
 import { useNavigate } from 'react-router';
 
 export interface ProfileInfosProps {
   picture: string;
   name: string;
   userId: string;
+  password: string;
 }
 
 export default function ProfileInfos({
   name,
   picture,
   userId,
+  password,
 }: ProfileInfosProps) {
+  const [hidePassword, setHidePassword] = useState(true);
   const { loginInfos } = LoginContext();
   const router = useNavigate();
+  const sameUser = loginInfos.id === userId;
 
   const handleEditProfile = () => {
     router(`/usuarios/${userId}`);
+  };
+
+  const handleShowPassword = () => {
+    setHidePassword(!hidePassword);
   };
   return (
     <div className="flex justify-between items-center gap-4 p-9">
@@ -33,15 +42,29 @@ export default function ProfileInfos({
           <h2 className="text-xl md:text-2xl lg:text-3xl dark:text-white tracking-wide font-bold font-bebas">
             {name}
           </h2>
+          {sameUser ? (
+            <span className="flex items-center gap-6 dark:text-sidebar-foreground font-bold tracking-wide text-sm lg:text-base">
+              Senha: {hidePassword ? '*********' : password}{' '}
+              <Button
+                onClick={handleShowPassword}
+                variant="ghost"
+                className="size-10 text-black dark:text-white rounded-full border-2 border-black dark:border-white"
+              >
+                {hidePassword ? <Eye /> : <EyeOff />}
+              </Button>
+            </span>
+          ) : null}
         </div>
       </div>
-      <Button
-        className="w-fit h-10 p-4 rounded-2xl bg-lime-600 hover:bg-lime-700 text-white dark:text-zinc-900 border-2 border-lime-600 dark:border-zinc-900 hidden md:flex"
-        disabled={loginInfos.id !== userId}
-        onClick={handleEditProfile}
-      >
-        Editar Perfil <Pencil />
-      </Button>
+      {sameUser ? (
+        <Button
+          className="w-fit h-10 p-4 rounded-2xl bg-lime-600 hover:bg-lime-700 text-white dark:text-zinc-900 border-2 border-lime-600 dark:border-zinc-900 hidden md:flex"
+          disabled={loginInfos.id !== userId}
+          onClick={handleEditProfile}
+        >
+          Editar Perfil <Pencil />
+        </Button>
+      ) : null}
     </div>
   );
 }
