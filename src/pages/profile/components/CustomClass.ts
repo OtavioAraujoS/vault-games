91d91cc8c703fc561b9a98e49b2f20c286a17480
otavio.silva <otavioaraujo490@gmail.com>
2025-03-28 10:07:08 -0300

    feat: add CustomClassObject for consistent styling in profile components

---
+++ src/pages/profile/components/CustomClass.ts
@@ -0,0 +1,10 @@
+import { CustomClass } from '@/pages/dashboard/components/GamesByStatus';
+
+export const CustomClassObject: CustomClass = {
+  iconClass: 'text-lime-600 dark:text-lime-600',
+  statusClass:
+    'flex justify-center items-center rounded-full bg-[#F2F2F2] dark:bg-[#212121] size-16 p-2',
+  labelClass: 'text-[1.2rem] font-semibold tracking-wider dark:text-white',
+  valueClass:
+    'text-center text-[1.5rem] text-lime-600 font-bebas dark:text-lime-600',
+};
