fcebae76191ac7c0894d04e3b3a7dc64ada4c48a
otavio.silva <otavioaraujo490@gmail.com>
2025-03-28 09:11:06 -0300

    feat: fetch and display user information in Profile component

--- src/pages/profile/Page.tsx
+++ src/pages/profile/Page.tsx
@@ -1,11 +1,34 @@
 import { Separator } from '@/components/ui/separator';
-import { LoginContext } from '@/context/LoginContext';
+import { useToast } from '@/hooks/use-toast';
+import { userService } from '@/services/user';
+import { UsersInfo } from '@/types/User';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
 import GameDistribuition from './components/GameDistribuition';
 import LastGamesUpdated from './components/LastGamesUpdated';
 import ProfileInfos from './components/ProfileInfos';
 
 export default function Profile() {
-  const { loginInfos } = LoginContext();
+  const [userInfos, setUserInfos] = useState<UsersInfo>();
+  const { id } = useParams();
+  const { toast } = useToast();
+
+  useEffect(() => {
+    if (!id) return;
+
+    const getUserInfos = async () => {
+      try {
+        const response = await userService.getUserById(id);
+        setUserInfos(response);
+      } catch {
+        toast({
+          title: 'Erro ao buscar informações do usuário',
+          description: 'Tente novamente mais tarde',
+        });
+      }
+    };
+    getUserInfos();
+  }, [id]);
   return (
     <div className="flex flex-col h-full min-h-screen w-full p-12 gap-8">
       <h1 className="border-l-4 border-lime-600 p-3 text-2xl md:text-3xl lg:text-4xl dark:text-white tracking-wide font-bold font-bebas">
@@ -14,9 +37,10 @@ export default function Profile() {
 
       <div className="flex flex-col p-4 gap-6 rounded-lg border-2 border-lime-600 bg-[#FAFAFA] dark:bg-[#212121]">
         <ProfileInfos
-          name={loginInfos.name}
-          picture={loginInfos.image}
-          userId={loginInfos.id}
+          name={userInfos?.nome || ''}
+          picture={userInfos?.picture || ''}
+          userId={userInfos?._id || ''}
+          password={userInfos?.password || ''}
         />
 
         <Separator className="bg-lime-600 dark:bg-lime-600" />
