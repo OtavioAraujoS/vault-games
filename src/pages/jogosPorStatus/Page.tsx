fad73f956f317e3aa99881953f8a21c86483f208
OtavioAraujoS <otavioaraujo490@gmail.com>
2025-10-30 11:47:04 -0300

    fix: enhance JogosPorStatus component with loading state and improve data fetching logic

--- src/pages/jogosPorStatus/Page.tsx
+++ src/pages/jogosPorStatus/Page.tsx
@@ -1,4 +1,7 @@
+'use client';
+
 import dayjs from 'dayjs';
+import { Suspense, useEffect, useState, useTransition } from 'react';
 import { StatusChip } from '@/components/StatusChip';
 import { TitlePage } from '@/components/TitlePage';
 import {
@@ -14,14 +17,16 @@ import {
   DialogHeader,
   DialogTitle,
 } from '@/components/ui/dialog';
+import { DialogTrigger } from '@radix-ui/react-dialog';
 import { LoginContext } from '@/context/LoginContext';
 import { useToast } from '@/hooks/use-toast';
 import { gameService } from '@/services/games';
 import { GamesByStatus } from '@/types/Games';
-import { DialogTrigger } from '@radix-ui/react-dialog';
-import { useEffect, useState } from 'react';
+import { Loader2 } from 'lucide-react';
+import { LoadingComponent } from '@/components/LoadingComponent';
 
 export const JogosPorStatus = () => {
+  const [isPending, startTransition] = useTransition();
   const [gamesByStatus, setGamesByStatus] = useState<GamesByStatus>({
     Completo: [],
     Progresso: [],
@@ -32,135 +37,143 @@ export const JogosPorStatus = () => {
   const { toast } = useToast();
 
   useEffect(() => {
-    const getGamesByUser = async () => {
-      try {
-        if (!loginInfos.id) {
-          return;
+    startTransition(() => {
+      void (async () => {
+        try {
+          if (!loginInfos?.id) return;
+
+          const response = await gameService.getGamesByUser(loginInfos.id);
+          setGamesByStatus(
+            response.reduce(
+              (acc: GamesByStatus, game) => {
+                acc[game.status].push(game);
+                return acc;
+              },
+              {
+                Completo: [],
+                Progresso: [],
+                Pausado: [],
+                Pendente: [],
+              }
+            )
+          );
+        } catch {
+          toast({
+            title: 'Erro ao buscar informações dos jogos',
+            description: 'Não foi possível buscar as informações do dashboard',
+            variant: 'destructive',
+          });
         }
-
-        const response = await gameService.getGamesByUser(loginInfos.id);
-        setGamesByStatus(
-          response.reduce(
-            (acc: GamesByStatus, game) => {
-              acc[game.status].push(game);
-              return acc;
-            },
-            {
-              Completo: [],
-              Progresso: [],
-              Pausado: [],
-              Pendente: [],
-            }
-          )
-        );
-      } catch {
-        toast({
-          title: 'Erro ao buscar informações do dashboard',
-          description: 'Não foi possível buscar as informações do dashboard',
-        });
-      }
-    };
-
-    getGamesByUser();
-  }, []);
+      })();
+    });
+  }, [loginInfos]);
 
   return (
-    <div className="flex flex-col h-full min-h-screen w-full p-12 gap-4">
-      <TitlePage title="Jogos Por Status" />
+    <Suspense fallback={<LoadingComponent />}>
+      {isPending ? (
+        <div className="flex flex-col items-center justify-center h-screen text-gray-500 dark:text-gray-400 gap-4 transition-opacity duration-300">
+          <Loader2 className="animate-spin size-10" />
+          <p>Buscando jogos por status...</p>
+        </div>
+      ) : (
+        <div className="flex flex-col h-full min-h-screen w-full p-12 gap-4 transition-opacity duration-500">
+          <TitlePage title="Jogos Por Status" />
 
-      {Object.keys(gamesByStatus).length > 0 ? (
-        <Accordion
-          type="multiple"
-          className="w-full"
-          defaultValue={['Completo', 'Progresso', 'Pausado', 'Pendente']}
-        >
-          {Object.keys(gamesByStatus)
-            .filter(
-              (status) =>
-                gamesByStatus[status as keyof GamesByStatus].length > 0
-            )
-            .map((status) => (
-              <AccordionItem key={status} value={status}>
-                <AccordionTrigger>
-                  <h2 className="text-2xl font-bold mb-4 border-l-red-500 border-l-4 p-2 dark:text-white">
-                    {status}
-                  </h2>
-                </AccordionTrigger>
-                <AccordionContent>
-                  <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-8 gap-4">
-                    {gamesByStatus[status as keyof GamesByStatus].map(
-                      (game) => (
-                        <Dialog key={game._id}>
-                          <DialogTrigger>
-                            <div
-                              key={game._id}
-                              className="bg-zinc-100 dark:bg-[#212121] p-4 rounded-md shadow-md overflow-hidden h-full max-w-[18rem]"
-                            >
-                              <img
-                                src={game.image}
-                                alt={game.nome}
-                                className="w-fit h-fit object-cover rounded-md"
-                              />
-                            </div>
-                          </DialogTrigger>
-                          <DialogContent className="h-fit min-w-[70rem] bg-white dark:bg-[#212121]">
-                            <DialogHeader>
-                              <DialogTitle className="text-black dark:text-white">
-                                Informações do Jogo
-                              </DialogTitle>
-                              <DialogDescription className="text-gray-800 dark:text-gray-400">
-                                Aqui você encontra informações sobre o jogo
-                              </DialogDescription>
-                            </DialogHeader>
-                            <div className="flex flex-col gap-6">
-                              <div className="flex flex-col gap-2 mt-2">
-                                <div className="flex justify-between items-center">
-                                  <h1 className="text-black dark:text-white text-2xl font-bold">
-                                    Título: {game.nome}
-                                  </h1>
+          {Object.values(gamesByStatus).some((arr) => arr.length > 0) ? (
+            <Accordion
+              type="multiple"
+              className="w-full"
+              defaultValue={['Completo', 'Progresso', 'Pausado', 'Pendente']}
+            >
+              {Object.keys(gamesByStatus)
+                .filter(
+                  (status) =>
+                    gamesByStatus[status as keyof GamesByStatus].length > 0
+                )
+                .map((status) => (
+                  <AccordionItem key={status} value={status}>
+                    <AccordionTrigger>
+                      <h2 className="text-2xl font-bold mb-4 border-l-red-500 border-l-4 p-2 dark:text-white">
+                        {status}
+                      </h2>
+                    </AccordionTrigger>
+                    <AccordionContent>
+                      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-8 gap-4">
+                        {gamesByStatus[status as keyof GamesByStatus].map(
+                          (game) => (
+                            <Dialog key={game._id}>
+                              <DialogTrigger>
+                                <div className="bg-zinc-100 dark:bg-[#212121] p-4 rounded-md shadow-md overflow-hidden h-full max-w-[18rem] cursor-pointer hover:scale-[1.02] transition-transform">
+                                  <img
+                                    src={game.image}
+                                    alt={game.nome}
+                                    className="w-fit h-fit object-cover rounded-md"
+                                  />
+                                </div>
+                              </DialogTrigger>
+                              <DialogContent className="h-fit min-w-[70rem] bg-white dark:bg-[#212121]">
+                                <DialogHeader>
+                                  <DialogTitle className="text-black dark:text-white">
+                                    Informações do Jogo
+                                  </DialogTitle>
+                                  <DialogDescription className="text-gray-800 dark:text-gray-400">
+                                    Aqui você encontra informações sobre o jogo
+                                  </DialogDescription>
+                                </DialogHeader>
+                                <div className="flex flex-col gap-6">
+                                  <div className="flex flex-col gap-2 mt-2">
+                                    <div className="flex justify-between items-center">
+                                      <h1 className="text-black dark:text-white text-2xl font-bold">
+                                        Título: {game.nome}
+                                      </h1>
+                                      <h1 className="text-black dark:text-white text-xl font-bold">
+                                        Criado em:{' '}
+                                        <strong className="text-yellow-600 dark:text-yellow-500">
+                                          {dayjs(game.createdAt).format(
+                                            'DD/MM/YYYY'
+                                          )}
+                                        </strong>
+                                      </h1>
+                                    </div>
+                                    <h1 className="text-gray-800 dark:text-gray-300 text-xl font-bold">
+                                      Descrição: {game.description}
+                                    </h1>
+                                  </div>
 
-                                  <h1 className="text-black dark:text-white text-xl font-bold">
-                                    Criado em:{' '}
-                                    <strong className="text-yellow-600 dark:text-yellow-500">
-                                      {dayjs(game.createdAt).format(
-                                        'DD/MM/YYYY'
-                                      )}
+                                  <div className="flex justify-between items-center">
+                                    <h1 className="text-black dark:text-white text-xl font-bold">
+                                      Tempo de jogo:{' '}
+                                      <strong className="text-yellow-600 dark:text-yellow-500">
+                                        {game.hours} horas
+                                      </strong>
+                                    </h1>
+                                    <StatusChip status={game.status} />
+                                  </div>
+
+                                  <h1 className="text-black dark:text-gray-300 text-xl font-bold">
+                                    Review:{' '}
+                                    <strong className="text-yellow-600 dark:text-yellow-500 tracking-wider text-2xl">
+                                      &ldquo;{game.review}&rdquo;
                                     </strong>
+                                    🤔💭💡🧠
                                   </h1>
                                 </div>
-                                <h1 className="text-gray-800 dark:text-gray-300 text-xl font-bold">
-                                  Descrição: {game.description}
-                                </h1>
-                              </div>
-                              <div className="flex justify-between items-center">
-                                <h1 className="text-black dark:text-white text-xl font-bold">
-                                  Tempo de jogo:{' '}
-                                  <strong className="text-yellow-600 dark:text-yellow-500">
-                                    {game.hours} horas
-                                  </strong>
-                                </h1>
-                                <StatusChip status={game.status} />
-                              </div>
-                              <h1 className="text-black dark:text-gray-300 text-xl font-bold">
-                                Review:{' '}
-                                <strong className="text-yellow-600 dark:text-yellow-500 tracking-wider text-2xl">
-                                  &ldquo;{game.review}&rdquo;
-                                </strong>
-                                🤔💭💡🧠
-                              </h1>
-                            </div>
-                          </DialogContent>
-                        </Dialog>
-                      )
-                    )}
-                  </div>
-                </AccordionContent>
-              </AccordionItem>
-            ))}
-        </Accordion>
-      ) : (
-        <p className="text-sm text-gray-500">Nenhum jogo encontrado.</p>
+                              </DialogContent>
+                            </Dialog>
+                          )
+                        )}
+                      </div>
+                    </AccordionContent>
+                  </AccordionItem>
+                ))}
+            </Accordion>
+          ) : (
+            <p className="text-sm text-gray-500 text-center mt-10">
+              Nenhum jogo encontrado.
+            </p>
+          )}
+        </div>
       )}
-    </div>
+    </Suspense>
   );
 };
