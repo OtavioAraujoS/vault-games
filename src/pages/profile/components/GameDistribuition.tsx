f3214fbb2c19d110adb7a7e45835d8b1fafc0857
OtavioAraujoS <otavioaraujo490@gmail.com>
2025-10-30 11:46:15 -0300

    fix: refactor GameDistribuition component to use Suspense and improve loading state handling

--- src/pages/profile/components/GameDistribuition.tsx
+++ src/pages/profile/components/GameDistribuition.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import {
   Card,
   CardContent,
@@ -13,9 +15,10 @@ import {
 } from '@/components/ui/chart';
 import { GameStatusDistribution } from '@/types/Dashboard';
 import { generateRandomColor } from '@/utils/GenerateRandomColor';
-import { useEffect, useState } from 'react';
+import { Suspense, useEffect, useState, useTransition } from 'react';
 import { Pie, PieChart } from 'recharts';
 import { chartConfig } from './ChartConfig';
+import { LoadingComponent } from '@/components/LoadingComponent';
 
 interface GameDistributionFormated {
   title: string;
@@ -30,73 +33,84 @@ interface GameDistribuitionProps {
 export default function GameDistribuition({
   gameDistributionList,
 }: GameDistribuitionProps) {
+  const [isPending, startTransition] = useTransition();
   const [gameDistribution, setGameDistribution] =
     useState<GameDistributionFormated[]>();
   const [resume, setResume] = useState<string>('');
 
   useEffect(() => {
     if (!gameDistributionList) return;
-    const formattedResponse: GameDistributionFormated[] = Object.entries(
-      gameDistributionList
-    ).map(([, value]) => ({
-      title: value.status,
-      quantidade: value.count as number,
-      fill: generateRandomColor(),
-    }));
 
-    setGameDistribution(formattedResponse);
+    startTransition(() => {
+      const formattedResponse: GameDistributionFormated[] = Object.entries(
+        gameDistributionList
+      ).map(([, value]) => ({
+        title: value.status,
+        quantidade: value.count as number,
+        fill: generateRandomColor(),
+      }));
 
-    const sortedGames = formattedResponse.sort(
-      (a, b) => b.quantidade - a.quantidade
-    );
+      setGameDistribution(formattedResponse);
 
-    const mostPlayed = sortedGames[0];
-    const leastPlayed = sortedGames[sortedGames.length - 1];
+      const sortedGames = formattedResponse.sort(
+        (a, b) => b.quantidade - a.quantidade
+      );
 
-    let message = `Você possui muitos jogos na categoria: "${mostPlayed.title}". `;
-    if (mostPlayed.title === 'Pendente') {
-      message += 'Tente focar em completar mais jogos!';
-    } else if (leastPlayed.title === 'Completo') {
-      message += 'Parabéns por completar muitos jogos, mas continue assim!';
-    } else {
-      message += `Considere dar mais atenção a seus jogos que estão: "${leastPlayed.title}".`;
-    }
+      const mostPlayed = sortedGames[0];
+      const leastPlayed = sortedGames[sortedGames.length - 1];
 
-    setResume(message);
+      let message = `Você possui muitos jogos na categoria: "${mostPlayed.title}". `;
+      if (mostPlayed.title === 'Pendente') {
+        message += 'Tente focar em completar mais jogos!';
+      } else if (leastPlayed.title === 'Completo') {
+        message += 'Parabéns por completar muitos jogos, mas continue assim!';
+      } else {
+        message += `Considere dar mais atenção a seus jogos que estão: "${leastPlayed.title}".`;
+      }
+
+      setResume(message);
+    });
   }, [gameDistributionList]);
-  return (
-    <div className="flex flex-col items-center gap-4 md:ml-8">
-      <h1 className="dark:text-white font-bebas text-center text-sm sm:text-lg md:text-xl lg:text-2xl">
-        Distribuição De Jogos por Status
-      </h1>
 
-      <Card className="flex flex-col w-full h-full max-w-[10rem] sm:min-w-[11rem] sm:max-w-[12rem] md:min-w-[15rem] md:max-w-[18rem] lg:min-w-[22rem] lg:max-w-[30rem]">
-        <CardHeader className="w-full max-w-[8rem] sm:max-w-[10rem] md:max-w-[16rem] lg:max-w-xs">
-          <CardTitle>Distribuição de Jogos</CardTitle>
-          <CardDescription>Filtro: Status</CardDescription>
-        </CardHeader>
-        <CardContent className="flex-1 p-10">
-          <ChartContainer
-            config={chartConfig}
-            className="mx-auto aspect-square max-h-[16rem] pb-0 [&_.recharts-pie-label-text]:fill-foreground"
-          >
-            <PieChart>
-              <ChartTooltip content={<ChartTooltipContent hideLabel />} />
-              <Pie
-                data={gameDistribution}
-                dataKey="quantidade"
-                label
-                nameKey="title"
-              />
-            </PieChart>
-          </ChartContainer>
-        </CardContent>
-        <CardFooter className="flex-col gap-2 text-sm">
-          <h1 className="flex items-center text-center gap-2 font-medium leading-none">
-            {resume}
+  return (
+    <Suspense fallback={<LoadingComponent />}>
+      {isPending || !gameDistribution ? (
+        <LoadingComponent />
+      ) : (
+        <div className="flex flex-col items-center gap-4 md:ml-8 transition-opacity duration-500">
+          <h1 className="dark:text-white font-bebas text-center text-sm sm:text-lg md:text-xl lg:text-2xl">
+            Distribuição De Jogos por Status
           </h1>
-        </CardFooter>
-      </Card>
-    </div>
+
+          <Card className="flex flex-col w-full h-full max-w-[10rem] sm:min-w-[11rem] sm:max-w-[12rem] md:min-w-[15rem] md:max-w-[18rem] lg:min-w-[22rem] lg:max-w-[30rem]">
+            <CardHeader className="w-full max-w-[8rem] sm:max-w-[10rem] md:max-w-[16rem] lg:max-w-xs">
+              <CardTitle>Distribuição de Jogos</CardTitle>
+              <CardDescription>Filtro: Status</CardDescription>
+            </CardHeader>
+            <CardContent className="flex-1 p-10">
+              <ChartContainer
+                config={chartConfig}
+                className="mx-auto aspect-square max-h-[16rem] pb-0 [&_.recharts-pie-label-text]:fill-foreground"
+              >
+                <PieChart>
+                  <ChartTooltip content={<ChartTooltipContent hideLabel />} />
+                  <Pie
+                    data={gameDistribution}
+                    dataKey="quantidade"
+                    label
+                    nameKey="title"
+                  />
+                </PieChart>
+              </ChartContainer>
+            </CardContent>
+            <CardFooter className="flex-col gap-2 text-sm">
+              <h1 className="flex items-center text-center gap-2 font-medium leading-none">
+                {resume}
+              </h1>
+            </CardFooter>
+          </Card>
+        </div>
+      )}
+    </Suspense>
   );
 }
