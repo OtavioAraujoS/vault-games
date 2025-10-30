da87b17647732145d355cf5b35bfd27c4b3f802e
OtavioAraujoS <otavioaraujo490@gmail.com>
2025-10-30 14:40:40 -0300

    fix: refactor Profile component to use Suspense for improved loading state and transition handling

--- src/pages/profile/Page.tsx
+++ src/pages/profile/Page.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import { Suspense, useEffect, useState, useTransition } from 'react';
 import { Separator } from '@/components/ui/separator';
 import { useToast } from '@/hooks/use-toast';
 import { gameService } from '@/services/games';
@@ -5,7 +8,6 @@ import { userService } from '@/services/user';
 import { GameStatusDistribution } from '@/types/Dashboard';
 import { GameStatus } from '@/types/Games';
 import { UsersInfo } from '@/types/User';
-import { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import { GamesByStatus } from '../dashboard/components/GamesByStatus';
 import { CustomClassObject } from './components/CustomClass';
@@ -13,12 +15,14 @@ import GameDistribuition from './components/GameDistribuition';
 import LastGamesUpdated from './components/LastGamesUpdated';
 import ProfileInfos from './components/ProfileInfos';
 import { LoginContext } from '@/context/LoginContext';
+import { LoadingComponent } from '@/components/LoadingComponent';
 
 export default function Profile() {
   const { loginInfos } = LoginContext();
   const [userInfos, setUserInfos] = useState<UsersInfo>();
   const [gameDistribution, setGameDistribution] =
     useState<GameStatusDistribution[]>();
+  const [isPending, startTransition] = useTransition();
   const { id } = useParams();
   const { toast } = useToast();
 
@@ -55,41 +59,50 @@ export default function Profile() {
 
   useEffect(() => {
     if (!id) return;
-
-    getUserInfos();
-    getGameDistribuitionByUserId();
+    startTransition(() => {
+      getUserInfos();
+      getGameDistribuitionByUserId();
+    });
   }, [id]);
+
   return (
-    <div className="flex flex-col h-full min-h-screen w-full p-12 gap-8">
-      <h1 className="border-l-4 border-lime-600 p-3 text-2xl md:text-3xl lg:text-4xl dark:text-white tracking-wide font-bold font-bebas">
-        Meu Perfil
-      </h1>
+    <Suspense fallback={<LoadingComponent />}>
+      <div className="flex flex-col h-full min-h-screen w-full p-12 gap-8">
+        <h1 className="border-l-4 border-lime-600 p-3 text-2xl md:text-3xl lg:text-4xl dark:text-white tracking-wide font-bold font-bebas">
+          Meu Perfil
+        </h1>
 
-      <div className="flex flex-col p-4 gap-12 rounded-lg border-2 border-lime-600 bg-[#FAFAFA] dark:bg-[#212121]">
-        <ProfileInfos
-          name={userInfos?.nome || ''}
-          picture={userInfos?.picture || ''}
-          userId={userInfos?._id || ''}
-          password={userInfos?.password || ''}
-        />
+        <div className="flex flex-col p-4 gap-12 rounded-lg border-2 border-lime-600 bg-[#FAFAFA] dark:bg-[#212121]">
+          {isPending ? (
+            <LoadingComponent />
+          ) : (
+            <>
+              <ProfileInfos
+                name={userInfos?.nome || ''}
+                picture={userInfos?.picture || ''}
+                userId={userInfos?._id || ''}
+                password={userInfos?.password || ''}
+              />
 
-        <Separator className="bg-lime-600 dark:bg-lime-600" />
+              <Separator className="bg-lime-600 dark:bg-lime-600" />
 
-        <GamesByStatus
-          gameStatusDistribution={
-            Array.isArray(gameDistribution) ? gameDistribution : []
-          }
-          customClass={CustomClassObject}
-        />
+              <GamesByStatus
+                gameStatusDistribution={
+                  Array.isArray(gameDistribution) ? gameDistribution : []
+                }
+                customClass={CustomClassObject}
+              />
 
-        <Separator className="bg-lime-600 dark:bg-lime-600" />
+              <Separator className="bg-lime-600 dark:bg-lime-600" />
 
-        <div className="w-full flex flex-wrap justify-evenly items-center gap-4">
-          <LastGamesUpdated />
-
-          <GameDistribuition gameDistributionList={gameDistribution} />
+              <div className="w-full flex flex-wrap justify-evenly items-center gap-4">
+                <LastGamesUpdated />
+                <GameDistribuition gameDistributionList={gameDistribution} />
+              </div>
+            </>
+          )}
         </div>
       </div>
-    </div>
+    </Suspense>
   );
 }
